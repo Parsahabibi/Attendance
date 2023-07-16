5a0f8dff8f1b274d03812df1b8521a29cd582949
Amirreza Kashipazha <amirrezakashipazha@gmaile.com>
2023-07-16 11:38:52 +0430

    return to request

--- resources/views/web/Layouts/side-bar.blade.php
+++ resources/views/web/Layouts/side-bar.blade.php
@@ -1,31 +1,31 @@
 <section class="side-bar col-2 col-sm-1 h-100 p-2 p-lg-3 d-flex flex-column justify-content-between align-items-center">
     <div class="mb-4">
-        <img src="./images/Nahira-logo.svg" alt="">
+        <img src="../images/Nahira-logo.svg" alt="">
     </div>
     <div class="bg-gray-500 p-2  item-sidebar col-11 col-md-8 active-item-sidebar"item-sidebar>
-        <img src="./images/home(3).svg" alt="" class="col-lg-7 img-dark-item-sidebar">
-        <img src="./images/home(1) 1.svg" alt="" class="col-lg-7 img-light-item-sidebar">
+        <img src="../images/home(3).svg" alt="" class="col-lg-7 img-dark-item-sidebar">
+        <img src="../images/home(1) 1.svg" alt="" class="col-lg-7 img-light-item-sidebar">
         <div class="dropdown-item-sidebar"></div>
     </div>
     <div class="bg-gray-500 p-2 col-11 col-md-8 item-sidebar"item-sidebar>
-        <img src="./images/workplace(2).svg" alt="" class="col-lg-7 img-dark-item-sidebar">
-        <img src="./images/workplace.svg" alt="" class="col-lg-7  img-light-item-sidebar">
+        <img src="../images/workplace(2).svg" alt="" class="col-lg-7 img-dark-item-sidebar">
+        <img src="../images/workplace.svg" alt="" class="col-lg-7  img-light-item-sidebar">
         <div class="dropdown-item-sidebar bg-white rounded-24 p-3 p-md-4">
             <h5 class="pb-3">محل کار</h5>
             <ul class="">
                 <li class="pb-3">
                     <a href="#" class="d-flex justify-content-between ">
                         لیست محل کار
-                        <img src="./images/down(2).svg" alt=""class="me-3 me-md-5">
-                        <img src="./images/down.svg" alt=""class="me-3 me-md-5">
+                        <img src="../images/down(2).svg" alt=""class="me-3 me-md-5">
+                        <img src="../images/down.svg" alt=""class="me-3 me-md-5">
 
                     </a>
                 </li>
                 <li class="pb-3">
                     <a href="#" class="d-flex justify-content-betwee n">
                         ثبت محل کار جدید
-                        <img src="./images/down(2).svg" alt=""class="me-3 me-md-5">
-                        <img src="./images/down.svg" alt=""class="me-3 me-md-5">
+                        <img src="../images/down(2).svg" alt=""class="me-3 me-md-5">
+                        <img src="../images/down.svg" alt=""class="me-3 me-md-5">
 
                     </a>
                 </li>
@@ -33,24 +33,24 @@
         </div>
     </div>
     <div class="bg-gray-500 p-2 col-11 col-md-8 item-sidebar"item-sidebar>
-        <img src="./images/working-hours(2).svg" alt="" class="col-lg-7 img-dark-item-sidebar">
-        <img src="./images/working-hours.svg" alt="" class="col-lg-7 img-light-item-sidebar">
+        <img src="../images/working-hours(2).svg" alt="" class="col-lg-7 img-dark-item-sidebar">
+        <img src="../images/working-hours.svg" alt="" class="col-lg-7 img-light-item-sidebar">
         <div class="dropdown-item-sidebar bg-white rounded-24 p-3 p-md-4">
             <h5 class="pb-3">شیفت</h5>
             <ul class="">
                 <li class="pb-3">
                     <a href="#" class="d-flex justify-content-between ">
                         لیست شیفت ها
-                        <img src="./images/down(2).svg" alt=""class="me-3 me-md-5">
-                        <img src="./images/down.svg" alt=""class="me-3 me-md-5">
+                        <img src="../images/down(2).svg" alt=""class="me-3 me-md-5">
+                        <img src="../images/down.svg" alt=""class="me-3 me-md-5">
 
                     </a>
                 </li>
                 <li class="pb-3">
                     <a href="#" class="d-flex justify-content-betwee n">
                         ثبت شیفت جدید
-                        <img src="./images/down(2).svg" alt=""class="me-3 me-md-5">
-                        <img src="./images/down.svg" alt=""class="me-3 me-md-5">
+                        <img src="../images/down(2).svg" alt=""class="me-3 me-md-5">
+                        <img src="../images/down.svg" alt=""class="me-3 me-md-5">
 
                     </a>
                 </li>
@@ -58,24 +58,24 @@
         </div>
     </div>
     <div class="bg-gray-500 p-2 col-11 col-md-8  item-sidebar"item-sidebar>
-        <img src="./images/user(20).svg" alt="" class="col-lg-7 img-dark-item-sidebar">
-        <img src="./images/user(19).svg" alt="" class="col-lg-7 img-light-item-sidebar">
+        <img src="../images/user(20).svg" alt="" class="col-lg-7 img-dark-item-sidebar">
+        <img src="../images/user(19).svg" alt="" class="col-lg-7 img-light-item-sidebar">
         <div class="dropdown-item-sidebar bg-white rounded-24 p-3 p-md-4">
             <h5 class="pb-3">پرسونل</h5>
             <ul class="">
                 <li class="pb-3">
                     <a href="#" class="d-flex justify-content-between ">
                         لیست پرسونل
-                        <img src="./images/down(2).svg" alt=""class="me-3 me-md-5">
-                        <img src="./images/down.svg" alt=""class="me-3 me-md-5">
+                        <img src="../images/down(2).svg" alt=""class="me-3 me-md-5">
+                        <img src="../images/down.svg" alt=""class="me-3 me-md-5">
 
                     </a>
                 </li>
                 <li class="pb-3">
                     <a href="#" class="d-flex justify-content-betwee n">
                         ثبت پرسونل جدید
-                        <img src="./images/down(2).svg" alt=""class="me-3 me-md-5">
-                        <img src="./images/down.svg" alt=""class="me-3 me-md-5">
+                        <img src="../images/down(2).svg" alt=""class="me-3 me-md-5">
+                        <img src="../images/down.svg" alt=""class="me-3 me-md-5">
 
                     </a>
                 </li>
@@ -83,72 +83,72 @@
         </div>
     </div>
     <div class="bg-gray-500 p-2 col-11 col-md-8 item-sidebar"item-sidebar>
-        <img src="./images/report(1).svg" alt="" class="col-lg-7 img-dark-item-sidebar">
-        <img src="./images/report.svg" alt="" class="col-lg-7  img-light-item-sidebar">
+        <img src="../images/report(1).svg" alt="" class="col-lg-7 img-dark-item-sidebar">
+        <img src="../images/report.svg" alt="" class="col-lg-7  img-light-item-sidebar">
         <div class="dropdown-item-sidebar bg-white rounded-24 p-3 p-md-4">
             <h5 class="pb-3">گزارش ها</h5>
             <ul class="">
                 <li class="pb-3">
                     <a href="#" class="d-flex justify-content-between ">
                         کارکرد
-                        <img src="./images/down(2).svg" alt=""class="me-3 me-md-5">
-                        <img src="./images/down.svg" alt=""class="me-3 me-md-5">
+                        <img src="../images/down(2).svg" alt=""class="me-3 me-md-5">
+                        <img src="../images/down.svg" alt=""class="me-3 me-md-5">
                     </a>
                 </li>
                 <li class="pb-3">
                     <a href="#" class="d-flex justify-content-between">
                         تردد
-                        <img src="./images/down(2).svg" alt=""class="me-3 me-md-5">
-                        <img src="./images/down.svg" alt=""class="me-3 me-md-5">
+                        <img src="../images/down(2).svg" alt=""class="me-3 me-md-5">
+                        <img src="../images/down.svg" alt=""class="me-3 me-md-5">
                     </a>
                 </li>
                 <li class="pb-3">
                     <a href="#" class="d-flex justify-content-between">
                         مرخصی
-                        <img src="./images/down(2).svg" alt=""class="me-3 me-md-5">
-                        <img src="./images/down.svg" alt=""class="me-3 me-md-5">
+                        <img src="../images/down(2).svg" alt=""class="me-3 me-md-5">
+                        <img src="../images/down.svg" alt=""class="me-3 me-md-5">
                     </a>
                 </li>
                 <li class="pb-3">
                     <a href="#" class="d-flex justify-content-between">
                         جزییات مرخصی
-                        <img src="./images/down(2).svg" alt=""class="me-3 me-md-5">
-                        <img src="./images/down.svg" alt=""class="me-3 me-md-5">
+                        <img src="../images/down(2).svg" alt=""class="me-3 me-md-5">
+                        <img src="../images/down.svg" alt=""class="me-3 me-md-5">
                     </a>
                 </li>
                 <li class="pb-3">
                     <a href="#" class="d-flex justify-content-between">
                         پروژه
-                        <img src="./images/down(2).svg" alt=""class="me-3 me-md-5">
-                        <img src="./images/down.svg" alt=""class="me-3 me-md-5">
+                        <img src="../images/down(2).svg" alt=""class="me-3 me-md-5">
+                        <img src="../images/down.svg" alt=""class="me-3 me-md-5">
                     </a>
                 </li>
             </ul>
         </div>
     </div>
     <div class="bg-gray-500 p-2  col-11 col-md-8 item-sidebar"item-sidebar>
-        <img src="./images/request(8).svg" alt="" class="col-lg-7 img-dark-item-sidebar">
-        <img src="./images/request(4).svg" alt="" class="col-lg-7 img-light-item-sidebar">
+        <img src="../images/request(8).svg" alt="" class="col-lg-7 img-dark-item-sidebar">
+        <img src="../images/request(4).svg" alt="" class="col-lg-7 img-light-item-sidebar">
         <div class="dropdown-item-sidebar"></div>
     </div>
     <div class="bg-gray-500 p-2 col-11 col-md-8 item-sidebar"item-sidebar>
-        <img src="./images/project-management(4).svg" alt="" class="col-lg-7 img-dark-item-sidebar">
-        <img src="./images/project-management(3).svg" alt="" class="col-lg-7 img-light-item-sidebar">
+        <img src="../images/project-management(4).svg" alt="" class="col-lg-7 img-dark-item-sidebar">
+        <img src="../images/project-management(3).svg" alt="" class="col-lg-7 img-light-item-sidebar">
         <div class="dropdown-item-sidebar bg-white rounded-24 p-3 p-md-4">
             <h5 class="pb-3">پروژه</h5>
             <ul class="">
                 <li class="pb-3">
                     <a href="#" class="d-flex justify-content-between ">
                         لیست پروژه
-                        <img src="./images/down(2).svg" alt=""class="me-3 me-md-5">
-                        <img src="./images/down.svg" alt=""class="me-3 me-md-5">
+                        <img src="../images/down(2).svg" alt=""class="me-3 me-md-5">
+                        <img src="../images/down.svg" alt=""class="me-3 me-md-5">
                     </a>
                 </li>
                 <li class="pb-3">
                     <a href="#" class="d-flex justify-content-betwee n">
                         ثبت پروژه جدید
-                        <img src="./images/down(2).svg" alt=""class="me-3 me-md-5">
-                        <img src="./images/down.svg" alt=""class="me-3 me-md-5">
+                        <img src="../images/down(2).svg" alt=""class="me-3 me-md-5">
+                        <img src="../images/down.svg" alt=""class="me-3 me-md-5">
                     </a>
                 </li>
 
@@ -156,11 +156,11 @@
         </div>
     </div>
     <div class="bg-gray-500 p-2 col-11 col-md-8 item-sidebar"item-sidebar>
-        <img src="./images/holiday(4).svg" alt="" class="col-lg-7 img-dark-item-sidebar">
-        <img src="./images/holiday(3).svg" alt="" class="col-lg-7  img-light-item-sidebar">
+        <img src="../images/holiday(4).svg" alt="" class="col-lg-7 img-dark-item-sidebar">
+        <img src="../images/holiday(3).svg" alt="" class="col-lg-7  img-light-item-sidebar">
         <div class="dropdown-item-sidebar"></div>
     </div>
     <div class="bg-gray-500 p-2 col-11 col-md-8  item-sidebar">
-        <img src="./images/logout(1).svg" alt="" class="col-lg-7">
+        <img src="../images/logout(1).svg" alt="" class="col-lg-7">
     </div>
 </section>
