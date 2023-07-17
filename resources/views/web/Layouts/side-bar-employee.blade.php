f66a97fc51ac85cad87e4b5d6e60dc6ab2d21165
Amirreza Kashipazha <amirrezakashipazha@gmaile.com>
2023-07-17 12:25:36 +0430

    return to shift

--- resources/views/web/Layouts/side-bar-employee.blade.php
+++ resources/views/web/Layouts/side-bar-employee.blade.php
@@ -1,16 +1,16 @@
 <section class="side-bar col-2 col-sm-1 h-100 p-2 p-lg-3 d-flex flex-column justify-content-between align-items-center">
     <div class="mb-4">
-        <img src="./images/Nahira-logo.svg" alt="">
+        <img src="../images/Nahira-logo.svg" alt="">
     </div>
      <div class="bg-gray-500 p-2  item-sidebar col-11 col-md-8 active-item-sidebar"item-sidebar>
-        <img src="./images/home(3).svg" alt="" class="col-lg-7 img-dark-item-sidebar">
-        <img src="./images/home(1) 1.svg" alt="" class="col-lg-7 img-light-item-sidebar">
+        <img src="../images/home(3).svg" alt="" class="col-lg-7 img-dark-item-sidebar">
+        <img src="../images/home(1) 1.svg" alt="" class="col-lg-7 img-light-item-sidebar">
         <div class="dropdown-item-sidebar"></div>
     </div>
    
     <div class="bg-gray-500 p-2 col-11 col-md-8 item-sidebar"item-sidebar>
-        <img src="./images/working-hours(2).svg" alt="" class="col-lg-7 img-dark-item-sidebar">
-        <img src="./images/working-hours.svg" alt="" class="col-lg-7 img-light-item-sidebar">
+        <img src="../images/working-hours(2).svg" alt="" class="col-lg-7 img-dark-item-sidebar">
+        <img src="../images/working-hours.svg" alt="" class="col-lg-7 img-light-item-sidebar">
         <div class="dropdown-item-sidebar bg-white rounded-24 p-3 p-md-4">
             <h5 class="pb-3">شیفت</h5>
             <ul class="">
@@ -34,28 +34,28 @@
         </div>
     </div>
     <div class="bg-gray-500 p-2 col-11 col-md-8 item-sidebar"item-sidebar>
-        <img src="./images/Frame 7.svg" alt="" class="col-lg-7 img-dark-item-sidebar">
-        <img src="./images/holiday(3).svg" alt="" class="col-lg-7  img-light-item-sidebar">
+        <img src="../images/Frame 7.svg" alt="" class="col-lg-7 img-dark-item-sidebar">
+        <img src="../images/holiday(3).svg" alt="" class="col-lg-7  img-light-item-sidebar">
         <div class="dropdown-item-sidebar"></div>
     </div> 
     <div class="bg-gray-500 p-2 col-11 col-md-8 item-sidebar"item-sidebar>
-        <img src="./images/Frame 7 (1).svg" alt="" class="col-lg-7 img-dark-item-sidebar">
-        <img src="./images/holiday(3).svg" alt="" class="col-lg-7  img-light-item-sidebar">
+        <img src="../images/Frame 7 (1).svg" alt="" class="col-lg-7 img-dark-item-sidebar">
+        <img src="../images/holiday(3).svg" alt="" class="col-lg-7  img-light-item-sidebar">
         <div class="dropdown-item-sidebar"></div>
     </div> 
     <div class="bg-gray-500 p-2  col-11 col-md-8 item-sidebar"item-sidebar>
-        <img src="./images/request(8).svg" alt="" class="col-lg-7 img-dark-item-sidebar">
-        <img src="./images/request(4).svg" alt="" class="col-lg-7 img-light-item-sidebar">
+        <img src="../images/request(8).svg" alt="" class="col-lg-7 img-dark-item-sidebar">
+        <img src="../images/request(4).svg" alt="" class="col-lg-7 img-light-item-sidebar">
         <div class="dropdown-item-sidebar"></div>
     </div>
    
     <div class="bg-gray-500 p-2 col-11 col-md-8 item-sidebar"item-sidebar>
-        <img src="./images/Frame 7 (2).svg" alt="" class="col-lg-7 img-dark-item-sidebar">
-        <img src="./images/holiday(3).svg" alt="" class="col-lg-7  img-light-item-sidebar">
+        <img src="../images/Frame 7 (2).svg" alt="" class="col-lg-7 img-dark-item-sidebar">
+        <img src="../images/holiday(3).svg" alt="" class="col-lg-7  img-light-item-sidebar">
         <div class="dropdown-item-sidebar"></div>
     </div> 
    
     <div class="bg-gray-500 p-2 col-11 col-md-8  item-sidebar">
-        <img src="./images/logout(1).svg" alt="" class="col-lg-7">
+        <img src="../images/logout(1).svg" alt="" class="col-lg-7">
     </div>
 </section>
