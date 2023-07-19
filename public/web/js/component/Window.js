3abcdb61de57a197493d338eb0a2856a027d580f
Amirreza Kashipazha <amirrezakashipazha@gmaile.com>
2023-07-19 23:48:45 +0430

    employee js

--- public/web/js/component/Window.js
+++ public/web/js/component/Window.js
@@ -1,18 +1,40 @@
-// import {
-//     DropdownItemSidebar,
-//     ItemSidebar,
-//     ItemSidebarImgDark,
-//     ItemSidebarImgLight,
-// } from "../Elements.js";
+import {
+    DataBtnDropDownOperation,
+    DataContentBtnDropDownOperation,
+    DataDropDownOperation,
+    DataDropdownDiv,
+    DataDropdownInput,
+    DataImageBtnDropDownOperation,
+    DropdownItemSidebar,
+    ItemSidebar,
+    ItemSidebarImgDark,
+    ItemSidebarImgLight,
+} from "../Elements.js";
 
-// window.onclick = (e) => {
-//     for (let i = 0; i < ItemSidebar.length; i++) {
-//         if (
-//             e.target !== ItemSidebarImgLight[i] &&
-//             e.target !== ItemSidebarImgDark[i] &&
-//             e.target !== ItemSidebar[i]
-//         ) {
-//             DropdownItemSidebar[i].style.display = "none";
-//         }
-//     }
-// };
+window.onclick = (e) => {
+    for (let i = 0; i < ItemSidebar.length; i++) {
+        if (
+            e.target !== ItemSidebarImgLight[i] &&
+            e.target !== ItemSidebarImgDark[i] &&
+            e.target !== ItemSidebar[i]
+        ) {
+            DropdownItemSidebar[i].style.display = "none";
+        }
+    }
+    for (let i = 0; i < DataDropdownInput.length; i++) {
+        if (e.target !== DataDropdownInput[i]) {
+            DataDropdownDiv[i].classList.add("d-none");
+        }
+    }
+
+    for (let i = 0; i < DataBtnDropDownOperation.length; i++) {
+        if (
+            e.target !== DataBtnDropDownOperation[i] &&
+            e.target !== DataContentBtnDropDownOperation[i] &&
+            e.target !== DataImageBtnDropDownOperation[i]
+        ) {
+            DataDropDownOperation[i].classList.add("d-none");
+        }
+    }
+};
+
